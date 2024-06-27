8eb7301289f6323e05eb3cb339fe28bdfe878bb7
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-06-27 11:07:55 +0300

    orientation

--- App.tsx
+++ App.tsx
@@ -1,18 +1,43 @@
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, SafeAreaView, Platform, ScrollView } from 'react-native';
+import { StyleSheet, SafeAreaView, Platform, ScrollView, Dimensions } from 'react-native';
 import PokemonCards from './components/PokemonCards';
 
 type PokemonProp = {
-  name: String,
-  image: any,
-  type: String,
-  hp: Number,
-  moves: Array<String>,
-  weakness: Array<String>
+  name: string;
+  image: any;
+  type: string;
+  hp: number;
+  moves: string[];
+  weakness: string[];
 };
 
-export default function App() {
-  const charm = {
+const App: React.FC = () => {
+  const [orientation, setOrientation] = useState<'portrait' | 'landscape'>('portrait');
+
+  useEffect(() => {
+    const updateOrientation = () => {
+      const { width, height } = Dimensions.get('window');
+      if (width > height) {
+        setOrientation('landscape');
+      } else {
+        setOrientation('portrait');
+      }
+    };
+
+    // Initial check
+    updateOrientation();
+
+    // Add event listener
+    Dimensions.addEventListener('change', updateOrientation);
+
+    // Cleanup event listener
+    return () => {
+      Dimensions.removeEventListener('change', updateOrientation);
+    };
+  }, []);
+
+  const charm: PokemonProp = {
     name: "Charm",
     image: require("./assets/image1.png"),
     type: "fire",
@@ -20,15 +45,15 @@ export default function App() {
     moves: ["scratch", "ember", "growl"],
     weakness: ["water", "air"]
   };
-  const squirel = {
+  const squirel: PokemonProp = {
     name: "Squirel",
     image: require("./assets/image2.png"),
-    type: "Water",
+    type: "water",
     hp: 39,
     moves: ["scratch", "ember", "growl"],
-    weakness: ["Fire", "air"]
+    weakness: ["fire", "air"]
   };
-  const Genye = {
+  const genye: PokemonProp = {
     name: "Genye",
     image: require("./assets/image3.png"),
     type: "grass",
@@ -36,7 +61,7 @@ export default function App() {
     moves: ["scratch", "ember", "growl"],
     weakness: ["wamarasta", "air"]
   };
-  const Cameni = {
+  const cameni: PokemonProp = {
     name: "Cameni",
     image: require("./assets/image4.png"),
     type: "electric",
@@ -48,15 +73,15 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
-          <PokemonCards {...charm} />
-      <PokemonCards {...squirel} />
-      <PokemonCards {...Genye} />
-      <PokemonCards {...Cameni} />
+        <PokemonCards {...charm} orientation={orientation} />
+        <PokemonCards {...squirel} orientation={orientation} />
+        <PokemonCards {...genye} orientation={orientation} />
+        <PokemonCards {...cameni} orientation={orientation} />
       </ScrollView>
-    
+      <StatusBar style="auto" />
     </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -65,3 +90,5 @@ const styles = StyleSheet.create({
     paddingTop: Platform.OS === 'android' ? 25 : 0,
   },
 });
+
+export default App;
