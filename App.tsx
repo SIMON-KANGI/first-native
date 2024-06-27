480853a190e3b4d289ff778096e5bd24c1093b37
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-06-27 10:46:48 +0300

    app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView, Platform, ScrollView } from 'react-native';
+import PokemonCards from './components/PokemonCards';
+
+type PokemonProp = {
+  name: String,
+  image: any,
+  type: String,
+  hp: Number,
+  moves: Array<String>,
+  weakness: Array<String>
+};
 
 export default function App() {
+  const charm = {
+    name: "Charm",
+    image: require("./assets/image1.png"),
+    type: "fire",
+    hp: 39,
+    moves: ["scratch", "ember", "growl"],
+    weakness: ["water", "air"]
+  };
+  const squirel = {
+    name: "Squirel",
+    image: require("./assets/image2.png"),
+    type: "Water",
+    hp: 39,
+    moves: ["scratch", "ember", "growl"],
+    weakness: ["Fire", "air"]
+  };
+  const Genye = {
+    name: "Genye",
+    image: require("./assets/image3.png"),
+    type: "grass",
+    hp: 39,
+    moves: ["scratch", "ember", "growl"],
+    weakness: ["wamarasta", "air"]
+  };
+  const Cameni = {
+    name: "Cameni",
+    image: require("./assets/image4.png"),
+    type: "electric",
+    hp: 39,
+    moves: ["scratch", "ember", "growl"],
+    weakness: ["stone", "air"]
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+          <PokemonCards {...charm} />
+      <PokemonCards {...squirel} />
+      <PokemonCards {...Genye} />
+      <PokemonCards {...Cameni} />
+      </ScrollView>
+    
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f5f5f5',
+    paddingTop: Platform.OS === 'android' ? 25 : 0,
   },
 });
